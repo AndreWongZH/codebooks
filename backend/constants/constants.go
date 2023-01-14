eec01b6cc356127acc2a8d271f779ef81e08110a
iamchenjiajun <iamchenjiajun@gmail.com>
2023-01-14 17:11:41 +0800

    fix language support

--- backend/constants/constants.go
+++ backend/constants/constants.go
@@ -1,9 +1,10 @@
 package constants
 
 var LanguageMap = map[string]string{
-	"c++":    "49",
-	"c":      "50",
-	"golang": "60",
+	"c++":     "54",
+	"c":       "50",
+	"golang":  "60",
+	"python3": "71",
 }
 
 const Judge0Endpoint = "http://localhost:2358"
