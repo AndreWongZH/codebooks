55ca877a631d67afbea2ab52dc0913f2308eacf0
iamchenjiajun <iamchenjiajun@gmail.com>
2023-01-14 20:38:39 +0800

    add check room exists endpoint

--- backend/main.go
+++ backend/main.go
@@ -50,6 +50,7 @@ func main() {
 	roomAPI.POST("/create", room.CreateRoom)
 	roomAPI.GET("/get", room.GetRoom)
 	roomAPI.POST("/save", room.SaveRoom)
+	roomAPI.GET("/check", room.CheckRoom)
 
 	// judge
 	judgeAPI := apiV1.Group("/judge")
